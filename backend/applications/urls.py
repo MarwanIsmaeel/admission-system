0d07e152bbe1f6a036b47f7e22c81e43978eb22d
MarwanIsmaeel <66996751+MarwanIsmaeel@users.noreply.github.com>
2026-09-08 00:51:17 +0300

    comment the url of dashboard-stats

--- backend/applications/urls.py
+++ backend/applications/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('api/initial-data/', views.initial_data, name='api_initial_data'),
     path('api/submit-application/', views.ApplicationCreateView.as_view(), name='api_submit_application'),
     # path('api/check-status/', views.check_status_api, name='api_check_status'),
-    path('api/dashboard-stats/', views.dashboard_stats, name='api_dashboard_stats'),
+    # path('api/dashboard-stats/', views.dashboard_stats, name='api_dashboard_stats'),
     
     # Utilities
     path('export-excel/', views.export_applications_excel, name='export_excel'),
